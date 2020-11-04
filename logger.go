c94624b8d53c88eb633067872c104f9cde1b617d
Luigi Zuccarelli <luigizuccarelli@gmail.com>
2020-11-04 08:42:14 +0000

    Update logger.go
    Removed line feed

--- logger.go
+++ logger.go
@@ -18,28 +18,28 @@ type Logger struct {
 
 func (logger Logger) Debug(message string) {
 	if logger.Level == DEBUG || logger.Level == TRACE {
-		log.Printf("\x1b[1;32m [%s] \x1b[0m : %s \n", "DEBUG", message)
+		log.Printf("\x1b[1;32m [%s] \x1b[0m : %s", "DEBUG", message)
 	}
 }
 
 func (logger Logger) Error(message string) {
-	log.Printf("\x1b[1;31m [%s] \x1b[0m : %s \n", "ERROR", message)
+	log.Printf("\x1b[1;31m [%s] \x1b[0m : %s", "ERROR", message)
 }
 
 func (logger Logger) Warn(message string) {
 	if logger.Level == WARN || logger.Level == DEBUG || logger.Level == INFO || logger.Level == TRACE {
-		log.Printf("\x1b[1;33m [%s] \x1b[0m  : %s \n", "WARN", message)
+		log.Printf("\x1b[1;33m [%s] \x1b[0m  : %s", "WARN", message)
 	}
 }
 
 func (logger Logger) Trace(message string) {
 	if logger.Level == TRACE {
-		log.Printf("\x1b[1;36m [%s] \x1b[0m : %s \n", "TRACE", message)
+		log.Printf("\x1b[1;36m [%s] \x1b[0m : %s", "TRACE", message)
 	}
 }
 
 func (logger Logger) Info(message string) {
 	if logger.Level == INFO || logger.Level == DEBUG || logger.Level == TRACE {
-		log.Printf("\x1b[1;34m [%s] \x1b[0m  : %s \n", "INFO", message)
+		log.Printf("\x1b[1;34m [%s] \x1b[0m  : %s", "INFO", message)
 	}
 }
